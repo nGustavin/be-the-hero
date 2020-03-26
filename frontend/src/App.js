8c05070b7b7684ff814a4c4e39063c7a120cdbcb
Gustavo Fernandes <gust4f3rn@gmail.com>
2020-03-26 03:34:17 -0300

    Front-end do app :D

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './global.css'
+
+import Routes from './routes';
+
+
+//Componetes em JS sao funções q retornam HTML(JSX JavaScript XML)
+export default function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes/>
   );
 }
 
-export default App;
+
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Profile() {
+    const [incidents, setIncidents] = useState([])
+
+    const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory();
+
+    useEffect(()=> {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId,
+            }
+        }).then(response => {
+            setIncidents(response.data)
+        })
+    }, [ongId])
+
+    async function handleDeleteIncident(id) {
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            })
+
+            setIncidents(incidents.filter(incidents => incidents.id !== id))
+        }catch(err) {
+            alert('Erro ao deletar caso, tente novamente')
+        }
+    }
+
+    function handleLogout() {
+        localStorage.clear()
+
+        history.push('/')
+    }
+
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be the Hero"/>
+                <span> Bem vinda, {ongName}</span>
+            
+                <Link className="button" to = "/incidents/new"> Cadastrar novo caso </Link>
+                <button onClick = {handleLogout} type = "button"> 
+                    <FiPower size={18} color = "#E02041"/>
+                </button>
+            </header>
+
+            <h1>Casos cadastrados: </h1>
+
+            <ul>
+                {incidents.map(incidents => (
+                    <li key={incidents.id}>
+                        <strong>CASO:</strong>
+                        <p>{incidents.title}</p>
+
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incidents.description}</p> 
+
+                        <strong>VALOR:</strong>
+                        <p>
+                            {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incidents.value)}
+                        
+                        </p>
+
+                        <button onClick = {() => handleDeleteIncident(incidents.id)} type = "button">
+                            <FiTrash2 size ={20} color ="#a8a8b3"/>
+                        </button>
+                    </li>
+
+                ))}
+            </ul>
+        </div>    
+    )
+}
